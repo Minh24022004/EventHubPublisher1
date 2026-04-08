8925bb76d17e2011955f01c2e1530f8992729b0e
Minh24022004 <minhdangtai2422004@gmail.com>
2026-04-08 10:37:06 +0700

    Add project files.

---
+++ EventHubConsumer/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.EventHubs.Producer;
+using Azure.Messaging.EventHubs;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+class program
+{
+  
+
+    static async Task Main()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+        string connectionString = config["EventHub:ConnectionString"];
+        string eventHubName = config["EventHub:EventHubName"];
+
+        await using var producerClient =
+            new EventHubProducerClient(connectionString, eventHubName);
+
+        while (true)
+        {
+            String message = Console.ReadLine();
+            if (message?.ToLower() == "exit")
+            {
+                Console.WriteLine("Exit...");
+                break;
+            }            
+            using EventDataBatch batch = await producerClient.CreateBatchAsync();
+
+            EventData eventData = new EventData(Encoding.UTF8.GetBytes(message));
+
+            if (batch.TryAdd(eventData))
+            {
+                await producerClient.SendAsync(batch);
+                Console.WriteLine($"Sent: {message}");
+            }
+        }
+    }
+    }
---
+++ EventHubPublisher/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Azure.Messaging.EventHubs.Producer;
+using Azure.Messaging.EventHubs;
+using System.Text;
+using Azure.Messaging.EventHubs.Consumer;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+
+    static async Task Main()
+    {
+        var config = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json", optional: false)
+           .Build();
+        string connectionString = config["EventHub:ConnectionString"];
+        string eventHubName = config["EventHub:EventHubName"];
+        string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+        await using var consumer = new EventHubConsumerClient(consumerGroup,connectionString);
+
+        Console.WriteLine("Listening for events...");
+
+        await foreach (PartitionEvent partitionEvent in consumer.ReadEventsAsync())
+        {
+            string message = Encoding.UTF8.GetString(partitionEvent.Data.Body.ToArray());
+
+            Console.WriteLine($"Received: {message}: {DateTime.Now} ");
+        }
+    }
+}
