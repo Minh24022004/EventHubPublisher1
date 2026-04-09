6009489e5ed3d2f882d6fd124fafbf63603c4347
Minh24022004 <minhdangtai2422004@gmail.com>
2026-04-09 10:19:11 +0700

    Step2 EventHubTest

--- EventHubConsumer/Program.cs
+++ EventHubConsumer/Program.cs
@@ -1,45 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Azure.Messaging.EventHubs.Producer;
 using Azure.Messaging.EventHubs;
-using System.Text;
+using Azure.Messaging.EventHubs.Producer;
 using Microsoft.Extensions.Configuration;
+using System.Text;
 
-class program
+class Program
 {
-  
-
     static async Task Main()
     {
         var config = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", optional: false)
             .Build();
+
         string connectionString = config["EventHub:ConnectionString"];
         string eventHubName = config["EventHub:EventHubName"];
 
         await using var producerClient =
             new EventHubProducerClient(connectionString, eventHubName);
 
+        Console.WriteLine("Type message, /spam to send 100 messages, /exit to quit");
+
+        int counter = 0;
+
         while (true)
         {
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            string message = Console.ReadLine();
 
-            if (keyInfo.Key == ConsoleKey.Escape)
+            if (message?.ToLower() == "/exit")
             {
                 Console.WriteLine("Exit...");
                 break;
             }
 
-            string message = keyInfo.KeyChar.ToString();
-
-            using EventDataBatch batch = await producerClient.CreateBatchAsync();
-
-            EventData eventData = new EventData(Encoding.UTF8.GetBytes(message));
-
-            if (batch.TryAdd(eventData))
+            if (message?.ToLower() == "/spam")
             {
+                using EventDataBatch batch = await producerClient.CreateBatchAsync();
+
+                for (int i = 0; i < 100; i++)
+                {
+                    string spamMessage = $"Spam message {counter} ";
+                    EventData eventData = new EventData(Encoding.UTF8.GetBytes(spamMessage));
+
+                    if (!batch.TryAdd(eventData))
+                        break;
+
+                    counter++;
+                }
+
                 await producerClient.SendAsync(batch);
-                Console.WriteLine($"Sent: {message}");
+                Console.WriteLine($"Sent 100 spam messages at {DateTime.Now}");
+            }
+            else
+            {
+                using EventDataBatch batch = await producerClient.CreateBatchAsync();
+
+                EventData eventData = new EventData(Encoding.UTF8.GetBytes(message));
+
+                if (batch.TryAdd(eventData))
+                {
+                    await producerClient.SendAsync(batch);
+                    Console.WriteLine($"Sent: {message}");
+                }
             }
         }
     }
-    }
+}
--- EventHubPublisher/Program.cs
+++ EventHubPublisher/Program.cs
@@ -1,32 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Azure.Messaging.EventHubs.Producer;
-using Azure.Messaging.EventHubs;
-using System.Text;
+﻿using Azure.Messaging.EventHubs;
 using Azure.Messaging.EventHubs.Consumer;
 using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Threading.Channels;
 
 class Program
 {
+    static DateTime lastProcessedTime = DateTime.MinValue;
+    static string checkpointFile = "checkpoint.txt";
 
     static async Task Main()
     {
         var config = new ConfigurationBuilder()
            .AddJsonFile("appsettings.json", optional: false)
            .Build();
+
         string connectionString = config["EventHub:ConnectionString"];
         string eventHubName = config["EventHub:EventHubName"];
-        string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
 
-        await using var consumer = new EventHubConsumerClient(consumerGroup,connectionString);
-
-        Console.WriteLine("Listening for events...");
-
-        await foreach (PartitionEvent partitionEvent in consumer.ReadEventsAsync())
+     
+        if (File.Exists(checkpointFile))
         {
-            string message = Encoding.UTF8.GetString(partitionEvent.Data.Body.ToArray());
+            string text = File.ReadAllText(checkpointFile);
+            if (DateTime.TryParse(text, out DateTime savedTime))
+            {
+                lastProcessedTime = savedTime;
+            }
+        }
 
-            Console.WriteLine($"Received: {message}");
+        await using var consumer = new EventHubConsumerClient(
+           EventHubConsumerClient.DefaultConsumerGroupName,
+           connectionString,
+           eventHubName);
+
+        Console.WriteLine($"Last processed time: {lastProcessedTime}");
+
+        var channel = Channel.CreateUnbounded<EventData>();
+
+        
+        for (int i = 0; i < 50; i++)
+        {
+            _ = Task.Run(async () =>
+            {
+                await foreach (var evt in channel.Reader.ReadAllAsync())
+                {
+                    string message = Encoding.UTF8.GetString(evt.Body.ToArray());
+                    DateTime eventTime = evt.EnqueuedTime.UtcDateTime;
+
+                    Console.WriteLine($"Processing: {message} + {DateTime.Now}");
+
+                    lastProcessedTime = eventTime;
+                    File.WriteAllText(checkpointFile, lastProcessedTime.ToString("O"));
+
+                    await Task.Delay(1000); 
+                }
+            });
+        }
+
+        EventPosition startPosition =
+            lastProcessedTime == DateTime.MinValue
+            ? EventPosition.Earliest
+            : EventPosition.FromEnqueuedTime(new DateTimeOffset(lastProcessedTime));
+
+        await foreach (PartitionEvent partitionEvent in
+            consumer.ReadEventsFromPartitionAsync("0", startPosition))
+        {
+            await channel.Writer.WriteAsync(partitionEvent.Data);          
         }
     }
 }
